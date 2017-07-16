8e3d52eb80cd03ed3c0a7a1d4357d0c9e100634e
Zack <zack@leanpro.cn>
2017-07-16 17:37:37 +0800

    fix

--- test_array.js
+++ test_array.js
@@ -1,79 +1,80 @@
-
+/**
+ * mocha selenium-webdriver demo
+ */
 require('chromedriver');
 var assert = require('assert');
 var webdriver = require('selenium-webdriver');
 var By = webdriver.By;
 var driver = new webdriver.Builder().forBrowser('chrome').build();
-var arr = ["hello world","hello john","good morning","good class"];
 
-describe('hooks', function() {
-    this.timeout(30*1000)
-  before(function() {
-      console.log('before...........')
-    driver.get('https://autowebtest.github.io/add-remove/');
-  });
+var arr = ["hello world", "hello john", "good morning", "good class"];
+// 声明 mocha
+describe('hooks', function () {
+    this.timeout(30 * 1000)   // 设置默认超时时间为30秒
+    before(function () {     // 所有test case运行之前
+        console.log('before ...........')
+        driver.get('https://autowebtest.github.io/add-remove/');
+    });
 
-  after(function(done) {
-      console.log("after..........");
-     driver.quit().then(function(){
-        return new Promise((resovle,reject)=>{
-            resovle();
-            
-        }).then(done);
-    })
-  });
-
-  beforeEach(function() {
-      console.log('beforeEach........')
-    // runs before each test in this block
-  });
-
-  afterEach(function(done) {
-      console.log("afterEach........")
-    driver.takeScreenshot().then((imagedata)=>{
-        var date = new Date();
-        let year = date.getFullYear();
-        let month = date.getMonth()+1;
-        let day = date.getDate();
-        let hour = date.getHours();
-        let min = date.getMinutes();
-        let sec = date.getSeconds();
-        let filename = year+"_"+month+"_"+day+"_"+hour+"_"+min+"_"+sec+".png"
-        require('fs').writeFileSync(filename,imagedata,'base64');
-        return new Promise((resovle,reject)=>{
-            resovle();
-            
-        }).then(done);
-    })
-  });
-
-  describe('#elemenet control', function() {
-    
-    it('add 4 element 长度应该为4', function(done) { 
-        arr.forEach((item)=>{
-            driver.findElement(By.id('text')).sendKeys(item);
-            driver.findElement(By.id('text')).submit(); 
-            
+    after(function (done) {    // 所有test case运行之后
+        console.log("after.........."); 
+        driver.quit().then(function () {
+            return new Promise((resovle, reject) => {
+                resovle();
+            }).then(done);
         })
-        driver.executeScript('document.querySelector(".clear-all").scrollIntoView()');
-        driver.findElements(By.className('new-item')).then(eles=>{
-            return new Promise(function(resovle,reject){
-                assert(4,eles.length);
-                resovle();
-            }).then(done);
-        });
     });
 
-describe('#elemenet control', function() {
-    it('shanchu', function(done) { 
-        driver.findElement(By.css('.clear-all')).click();
-        driver.switchTo().alert().accept().then(()=>{
-            return new Promise(function(resovle,reject){
-                resovle();
-            }).then(done);
-        });
+    beforeEach(function () {     //每一个test case 运行之前
+        console.log('beforeEach........')
+        // runs before each test in this block
     });
-  });
-  });
+
+    afterEach(function (done) {   // 每一个test case 运行之后
+        console.log("afterEach........")
+        driver.takeScreenshot().then((imagedata) => {
+            var date = new Date();
+            let year = date.getFullYear();
+            let month = date.getMonth() + 1;
+            let day = date.getDate();
+            let hour = date.getHours();
+            let min = date.getMinutes();
+            let sec = date.getSeconds();
+            let filename = year + "_" + month + "_" + day + "_" + hour + "_" + min + "_" + sec + ".png"   //获取到当前时间，存储为文件名。
+            require('fs').writeFileSync(filename, imagedata, 'base64');   //写入到截屏
+            return new Promise((resovle, reject) => {
+                resovle();
+
+            }).then(done);
+        })
+    });
+
+    describe('#elemenet control', function () {
+        // https://autowebtest.github.io/add-remove/   增加4个item，应该看到有4个值
+        it('添加4个元素, 长度应该为4', function (done) {
+            arr.forEach((item) => {
+                driver.findElement(By.id('text')).sendKeys(item);
+                driver.findElement(By.id('text')).submit();
+            })
+            driver.executeScript('document.querySelector(".clear-all").scrollIntoView()');
+            driver.findElements(By.className('new-item')).then(eles => {
+                return new Promise(function (resovle, reject) {
+                    assert(4, eles.length);
+                    resovle();
+                }).then(done);
+            });
+        });
+
+        //删除所有元素
+        it('删除元素', function (done) {
+            driver.findElement(By.css('.clear-all')).click();
+            driver.switchTo().alert().accept().then(() => {
+                return new Promise(function (resovle, reject) {
+                    resovle();
+                }).then(done);
+            });
+        });
+        
+    });
 
 });
